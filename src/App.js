a4e7fb835bbe628783fc72510bc2880ec94fbb42
Mr. Darkside <rachel@Mrs-MacBook-Pro.local>
2020-08-26 17:07:44 -0500

    Updated App.js with new Spotify code

--- src/App.js
+++ src/App.js
@@ -1,61 +1,55 @@
-import React from 'react';
-import hash from './hash';
-import logo from './logo.svg';
-import './App.css';
-import * as $ from 'jquery';
+import React, { Component } from "react";
+import hash from "./hash";
+import logo from "./logo.svg";
+import "./App.css";
+import * as $ from "jquery";
 import Player from "./Player";
 
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
-
-const clientID = "d8bcda8188d3434eb0fa1eae35c25663";
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = "YOUR_CLIENT_ID_GOES_HERE";
 const redirectUri = "http://localhost:3000";
 const scopes = [
   "user-read-currently-playing",
-  "user-read-playback-state"
+  "user-read-playback-state",
 ];
-
-//Get has of the URL
+// Get the hash of the url
 const hash = window.location.hash
   .substring(1)
   .split("&")
-  .reduce((initial, item) => {
+  .reduce(function(initial, item) {
     if (item) {
-      let parts = item.split("=");
-      initial[parts[0]] = decodeURI(parts[1]);
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
     }
     return initial;
   }, {});
-
-  window.location.hash = "";
-
-
+window.location.hash = "";
 class App extends Component {
   constructor() {
     super();
     this.state = {
-        token: null,
-      item: {
-        album: {
-          images: [{ url: ""}]
-        },
-        name: "",
-        artists: [{ name: ""}],
-        duration_ms: 0,
+      token: null,
+    item: {
+      album: {
+        images: [{ url: "" }]
       },
-      is_playing: "Paused",
-      progress_ms: 0
-    };
-
-    this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
+      name: "",
+      artists: [{ name: "" }],
+      duration_ms:0,
+    },
+    is_playing: "Paused",
+    progress_ms: 0
+  };
+  this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
   }
-
   getCurrentlyPlaying(token) {
-    //Make a call using token
+    // Make a call using the token
     $.ajax({
       url: "https://api.spotify.com/v1/me/player",
       type: "GET",
       beforeSend: (xhr) => {
-        xhr.setRequestHeader("Authorization", "Bearer" + token);
+        xhr.setRequestHeader("Authorization", "Bearer " + token);
       },
       success: (data) => {
         this.setState({
@@ -64,36 +58,38 @@ class App extends Component {
           progress_ms: data.progress_ms,
         });
       }
-    })
+    });
   }
 
   componentDidMount() {
-    //Set token
+    // Set token
     let _token = hash.access_token;
     if (_token) {
+      // Set token
       this.setState({
         token: _token
       });
     }
   }
-
-  render() {
-      return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-
-            {!this.state.token && (
-              <a className="btn btn--loginApp-link" href={`${authEndpoint}client_id=${clientID}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}>Login to local Spotify</a>
-            )}
-
-            {this.state.token && (
-              //Spotify player here
-            )}
-          </header>
-        </div>
-      );
+render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      {!this.state.token && (
+        <a
+          className="btn btn--loginApp-link"
+          href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+        >
+          Login to Spotify
+        </a>
+      )}
+      {this.state.token && (
+        // Spotify Player Will Go Here In the Next Step
+      )}
+      </header>
+    </div>
+  );
   }
 }
-
 export default App;
